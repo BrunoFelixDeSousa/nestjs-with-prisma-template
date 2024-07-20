79903036fdebc5dcfb4572ecf247c7db26f8f6da
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-07-19 22:18:46 -0300

    add comentarios no env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,12 @@
-import { z } from 'zod';
+import { z } from 'zod'; // Importa a biblioteca 'zod' para validação de esquemas.
 
 export const envSchema = z
   .object({
-    DATABASE_URL: z.string().url(),
-    JWT_PRIVATE_KEY: z.string(),
-    JWT_PUBLIC_KEY: z.string(),
-    PORT: z.coerce.number().optional().default(3333),
+    DATABASE_URL: z.string().url(), // Define que a variável de ambiente 'DATABASE_URL' deve ser uma string contendo uma URL válida.
+    JWT_PRIVATE_KEY: z.string(), // Define que a variável de ambiente 'JWT_PRIVATE_KEY' deve ser uma string.
+    JWT_PUBLIC_KEY: z.string(), // Define que a variável de ambiente 'JWT_PUBLIC_KEY' deve ser uma string.
+    PORT: z.coerce.number().optional().default(3333), // Define que a variável de ambiente 'PORT' deve ser um número opcional, com valor padrão de 3333.
   })
-  .required();
+  .required(); // Indica que todas as propriedades do objeto são obrigatórias.
 
-export type Env = z.infer<typeof envSchema>;
+export type Env = z.infer<typeof envSchema>; // Define um tipo 'Env' que é inferido a partir do schema 'envSchema'.
