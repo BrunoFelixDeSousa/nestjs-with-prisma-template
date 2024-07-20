8713bf127cd82a7d2efc9d3b984ebfd1b5005759
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-07-19 22:29:00 -0300

    add comentarios no zod-validation

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,24 +1,28 @@
-import { PipeTransform, BadRequestException } from '@nestjs/common';
-import { ZodError, ZodSchema } from 'zod';
-import { fromZodError } from 'zod-validation-error';
+import { PipeTransform, BadRequestException } from '@nestjs/common'; // Importa as interfaces 'PipeTransform' e 'BadRequestException' do pacote '@nestjs/common'.
+import { ZodError, ZodSchema } from 'zod'; // Importa as classes 'ZodError' e 'ZodSchema' do pacote 'zod'.
+import { fromZodError } from 'zod-validation-error'; // Importa a função 'fromZodError' do pacote 'zod-validation-error'.
 
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodSchema) {}
+  // Define uma classe 'ZodValidationPipe' que implementa a interface 'PipeTransform'.
+  constructor(private schema: ZodSchema) {} // Construtor que aceita um schema do tipo 'ZodSchema' e o armazena na propriedade 'schema'.
 
   transform(value: unknown) {
+    // Método 'transform' que recebe um valor desconhecido.
     try {
-      const parsedValue = this.schema.parse(value);
-      return parsedValue;
+      const parsedValue = this.schema.parse(value); // Tenta parsear o valor usando o schema fornecido.
+      return parsedValue; // Retorna o valor parseado se não houver erros.
     } catch (error) {
       if (error instanceof ZodError) {
+        // Verifica se o erro é uma instância de 'ZodError'.
         throw new BadRequestException({
+          // Lança uma exceção 'BadRequestException' com detalhes do erro de validação.
           message: 'Validation failed',
           statusCode: 400,
-          error: fromZodError(error),
+          error: fromZodError(error), // Usa 'fromZodError' para formatar o erro.
         });
       }
 
-      throw new BadRequestException('Validation failed');
+      throw new BadRequestException('Validation failed'); // Lança uma exceção genérica 'BadRequestException' se o erro não for um 'ZodError'.
     }
   }
 }
