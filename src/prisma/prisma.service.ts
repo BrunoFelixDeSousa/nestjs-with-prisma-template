88f25e8b73209239a0264cffe64b45c81af04e86
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-07-19 22:32:01 -0300

    add comentarios no prisma.service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,22 +1,24 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'; // Importa a classe 'PrismaClient' do pacote '@prisma/client'.
 
 @Injectable()
+// Estende a classe 'PrismaClient' para herdar suas funcionalidades.
+// Implementa as interfaces 'OnModuleInit' e 'OnModuleDestroy'.
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
     super({
-      log: ['error', 'info', 'query', 'warn'],
+      log: ['error', 'info', 'query', 'warn'], // Configura o PrismaClient para registrar diferentes níveis de log.
     });
   }
 
   onModuleInit() {
-    return this.$connect();
+    return this.$connect(); // Método chamado quando o módulo é inicializado; conecta o PrismaClient ao banco de dados.
   }
 
   onModuleDestroy() {
-    return this.$disconnect();
+    return this.$disconnect(); // Método chamado quando o módulo é destruído; desconecta o PrismaClient do banco de dados.
   }
 }
