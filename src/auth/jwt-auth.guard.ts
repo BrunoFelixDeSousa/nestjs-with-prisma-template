bccaf0f71c2d3594db813ab1788966f0d6eee426
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-08-09 14:46:33 -0300

    add comentários

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,3 +1,4 @@
 import { AuthGuard } from '@nestjs/passport';
 
+// Cria uma classe de guarda de autenticação utilizando a estratégia 'jwt' definida anteriormente
 export class JwtAuthGuard extends AuthGuard('jwt') {}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,27 +3,34 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Env } from 'src/env';
-import { string, z } from 'zod';
+import { z } from 'zod';
 
+// Definindo um schema de validação para o payload do JWT usando Zod
 const tokenSchema = z.object({
-  sub: string().uuid(),
+  sub: z.string().uuid(), // O campo 'sub' deve ser uma string com formato UUID
 });
 
+// Tipagem para o schema de token, baseada na inferência de tipos do Zod
 type TokeSchema = z.infer<typeof tokenSchema>;
 
 @Injectable()
+// Implementação da estratégia JWT com a biblioteca Passport
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(config: ConfigService<Env, true>) {
+    // Recupera a chave pública do JWT a partir das variáveis de ambiente
     const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
 
+    // Configura a estratégia JWT
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: Buffer.from(publicKey, 'base64'),
-      algorithms: ['RS256'],
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Extrai o JWT do cabeçalho de autorização
+      secretOrKey: Buffer.from(publicKey, 'base64'), // Decodifica a chave pública de base64 para um buffer
+      algorithms: ['RS256'], // Define o algoritmo de assinatura como RS256
     });
   }
 
+  // Método chamado automaticamente para validar o payload do JWT
   async validate(payload: TokeSchema) {
+    // Valida o payload do token de acordo com o schema definido
     return tokenSchema.parse(payload);
   }
 }
