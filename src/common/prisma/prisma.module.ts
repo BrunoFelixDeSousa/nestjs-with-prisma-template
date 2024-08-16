535c15f77749cb33ca4356fabacbc03663cad025
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-08-16 01:05:42 -0300

    add decorator @Global

--- src/common/prisma/prisma.module.ts
+++ src/common/prisma/prisma.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService], // Exporta o PrismaService para que outros módulos possam usá-lo
